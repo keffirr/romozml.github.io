618f50345944e1aabedba93a07649a4b94a183d7
RomozML <81045088+RomozML@users.noreply.github.com>
2021-03-31 21:05:48 +0300

    Create handler.php

---
+++ discord/handler.php
@@ -0,0 +1,6 @@
+  <p><b>лох &quot;ОС&quot;?</b></p>
+  <p><input type="radio" name="answer" value="a1">лох<Br>
+  <input type="radio" name="answer" value="a2">лох<Br>
+  <input type="radio" name="answer" value="a3">лох</p>
+  <p><input type="submit"></p>
+ </form>
